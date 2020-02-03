9431af49f26e08d610a748af8844688ac7bff6fd
PookMook <arthur.juchereau@gmail.com>
2020-02-02 20:35:57 -0500

    fixing being an idiot on a sunday night

--- frontend/src/helpers/env.js
+++ frontend/src/helpers/env.js
@@ -3,14 +3,14 @@
 let env = {}
 
 if(process.env.NODE_ENV === "production"){
-  env.REACT_APP_BACKEND = "localhost:4000"
-  env.REACT_APP_PROTOCOL = "http"
-  env.REACT_APP_SOCKET_PROTOCOL= "ws"
+  env.REACT_APP_BACKEND = "graphql.arthurdev.duckdns.org"
+  env.REACT_APP_PROTOCOL = "https"
+  env.REACT_APP_SOCKET_PROTOCOL= "wss"
 }
 else{
-  env.REACT_APP_BACKEND = process.env.REACT_APP_BACKEND || "graphql.arthurdev.duckdns.org"
-  env.REACT_APP_PROTOCOL = process.env.REACT_APP_PROTOCOL || "https"
-  env.REACT_APP_SOCKET_PROTOCOL= process.env.REACT_APP_SOCKET_PROTOCOL || "wss"
+  env.REACT_APP_BACKEND = process.env.REACT_APP_BACKEND || "localhost:4000"
+  env.REACT_APP_PROTOCOL = process.env.REACT_APP_PROTOCOL || "http"
+  env.REACT_APP_SOCKET_PROTOCOL= process.env.REACT_APP_SOCKET_PROTOCOL || "ws"
 }
 
 export default env
