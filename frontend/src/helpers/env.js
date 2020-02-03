f3f1583b43e614d725d98bd2d5c4a56d14dcd6d3
PookMook <arthur.juchereau@gmail.com>
2020-02-02 20:32:18 -0500

    update example to duckdns

--- frontend/src/helpers/env.js
+++ frontend/src/helpers/env.js
@@ -8,9 +8,9 @@ if(process.env.NODE_ENV === "production"){
   env.REACT_APP_SOCKET_PROTOCOL= "ws"
 }
 else{
-  env.REACT_APP_BACKEND = process.env.REACT_APP_BACKEND || "localhost:3080"
-  env.REACT_APP_PROTOCOL = process.env.REACT_APP_PROTOCOL || "http"
-  env.REACT_APP_SOCKET_PROTOCOL= process.env.REACT_APP_SOCKET_PROTOCOL || "ws"
+  env.REACT_APP_BACKEND = process.env.REACT_APP_BACKEND || "graphql.arthurdev.duckdns.org"
+  env.REACT_APP_PROTOCOL = process.env.REACT_APP_PROTOCOL || "https"
+  env.REACT_APP_SOCKET_PROTOCOL= process.env.REACT_APP_SOCKET_PROTOCOL || "wss"
 }
 
 export default env
